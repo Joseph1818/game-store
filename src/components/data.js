799c2834c296864c8ba3be323f8d1696878f326f
Umba joseph <Josephdjamba04@gmail.com>
2024-08-20 11:13:26 +0200

    Fix/cart icon and login color

--- src/components/data.js
+++ src/components/data.js
@@ -25,13 +25,13 @@ export const productData = [
     imageurl:
       "https://media.takealot.com/covers_images/c7edd9effb994fdca78db8cbe3c00fd7/s-pdpxl.file",
     name: "JBL Boombox ",
-    price: "$499.99",
+    price: "R3999.99",
   },
   {
     id: 3,
     imageurl:
       "https://m.media-amazon.com/images/I/51LCwQxwLEL._AC_UF1000,1000_QL80_.jpg",
-    price: "$99.99",
+    price: "R1799.99",
     name: "White PS5 Headset",
   },
   {
@@ -39,21 +39,21 @@ export const productData = [
     imageurl:
       "https://media.takealot.com/covers_tsins/35537449/5026555284080-1-pdpxl.jpg",
     name: "XBOX ONE GTA 5",
-    price: "$149.99",
+    price: "R1899.99",
   },
   {
     id: 5,
     imageurl:
       "https://www.soundandcinema.co.za/wp-content/uploads/2022/09/JBL_BOOMBOX_2_HERO_020_x1.jpg",
     name: "JBL Boombox 2",
-    price: "$499.99",
+    price: "R3999.99",
   },
   {
     id: 4,
     imageurl:
       "https://multimedia.bbycastatic.ca/multimedia/products/400x400/175/17553/17553188.jpg",
     name: " The Last of Us remast...",
-    price: "$49.99",
+    price: "R699.99",
   },
 
   {
@@ -61,21 +61,21 @@ export const productData = [
     imageurl:
       "https://media.takealot.com/covers_images/c7edd9effb994fdca78db8cbe3c00fd7/s-pdpxl.file",
     name: "JBL Boombox ",
-    price: "$499.99",
+    price: "R3999.99",
   },
   {
     id: 8,
     imageurl:
       "https://cdn.awsli.com.br/600x450/365/365753/produto/2487761517899208571.jpg",
     name: "FC 24 PS5 New ",
-    price: "$99.99",
+    price: "R1899.99",
   },
   {
     id: 2,
     imageurl:
       "https://5.imimg.com/data5/SELLER/Default/2023/6/317136830/LY/TG/CK/97262289/xbox-core-wireless-controller-carbon-black-500x500.jpg",
     name: "Xbox Black controller",
-    price: "$79.99",
+    price: "R1899.99",
   },
 ];
 
@@ -85,56 +85,56 @@ export const ProductDataPs5 = [
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6551/6551242_sd.jpg;maxHeight=640;maxWidth=550",
     name: "FC 24 PS5",
-    price: "$99.99",
+    price: "R1799.99",
   },
   {
     id: 2,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6579/6579203_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Terminator PS5",
-    price: "$89.99",
+    price: "R999.99",
   },
   {
     id: 3,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6503/6503672_sd.jpg;maxHeight=640;maxWidth=550",
     name: "GTA PS5",
-    price: "$79.99",
+    price: "R1899",
   },
   {
     id: 4,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6550/6550657_sd.jpg;maxHeight=640;maxWidth=550",
     name: "2K 24",
-    price: "$89.99",
+    price: "R1599",
   },
   {
     id: 5,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6558/6558585_sd.jpg;maxHeight=640;maxWidth=550",
     name: "COD MODERN W",
-    price: "$79.99",
+    price: "R1799.99",
   },
   {
     id: 6,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6566/6566050_sd.jpg;maxHeight=640;maxWidth=550",
     name: "SpiderMan Ps5",
-    price: "$89.99",
+    price: "R1499.99",
   },
   {
     id: 7,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6536/6536644_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Robocop PS5",
-    price: "$59.99",
+    price: "R899.99",
   },
   {
     id: 8,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6543/6543699_sd.jpg;maxHeight=640;maxWidth=550",
     name: "F1 Rally",
-    price: "$89.99",
+    price: "R899.99",
   },
 ];
 
@@ -144,28 +144,28 @@ export const ProductPs5Controller = [
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6430/6430163_sd.jpg;maxHeight=640;maxWidth=550",
     name: "WHITE Controller PS5",
-    price: "$89.99",
+    price: "R1799.99",
   },
   {
     id: 3,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6492/6492317_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Blue PS4",
-    price: "$99.99",
+    price: "R1899.99",
   },
   {
     id: 4,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6497/6497953_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Galactic Purple PS5",
-    price: "$99.99",
+    price: "R1799.99",
   },
   {
     id: 5,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6522/6522931_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Camo PS5",
-    price: "$99.99",
+    price: "R1799.99",
   },
 ];
 
@@ -175,70 +175,70 @@ export const ProdcutPs4Game = [
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6259/6259297_sd.jpg;maxHeight=640;maxWidth=550",
     name: "GTA PS4",
-    price: "$99.99",
+    price: "R1799.99",
   },
   {
     id: 2,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6444/6444455_sd.jpg;maxHeight=640;maxWidth=550",
     name: "It takes 2 PS4",
-    price: "$19.99",
+    price: "R299.99",
   },
   {
     id: 3,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6540/6540862_sd.jpg;maxHeight=640;maxWidth=550",
     name: "CRASH TEAM PS4",
-    price: "$79.99",
+    price: "R899.99",
   },
   {
     id: 4,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6517/6517774_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Dead Island 2",
-    price: "$69.99",
+    price: "R799.99",
   },
   {
     id: 5,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/4880/4880100_sa.jpg;maxHeight=640;maxWidth=550",
     name: "Mortal Kombat ",
-    price: "$99.99",
+    price: "R1899.99",
   },
   {
     id: 6,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5875/5875178_sd.jpg;maxHeight=640;maxWidth=550",
     name: "T LAST OF US PS4",
-    price: "$19.99",
+    price: "R1799.99",
   },
   {
     id: 7,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6501/6501072_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Grand thurismo 7",
-    price: "$99.99",
+    price: "R1499.99",
   },
   {
     id: 8,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5358/5358601_rd.jpg;maxHeight=640;maxWidth=550",
     name: "Days Gone PS4",
-    price: "$19.99",
+    price: "R1799.99",
   },
   {
     id: 9,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/8539/8539004_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Until Dawn Three PS4",
-    price: "$99.99",
+    price: "R1499.99",
   },
   {
     id: 10,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6549/6549701_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Avatar PS4",
-    price: "$99.99",
+    price: "R1799.99",
   },
 ];
 
@@ -248,62 +248,62 @@ export const ProductPs4Controller = [
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6200/6200230_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Navy Controller PS4",
-    price: "$79.99",
+    price: "R1799.99",
   },
   {
     id: 2,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5580/5580915_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Black PS4 Controller",
-    price: "$79.99",
+    price: "R1299.99",
   },
   {
     id: 3,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6381/6381722_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Red PS4 ",
-    price: "$79.99",
+    price: "R1499.99",
   },
   {
     id: 4,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6393/6393259_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Geek Squad PS4",
-    price: "$99.99",
+    price: "R1499.99",
   },
   {
     id: 5,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6381/6381885_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Gold PS4",
-    price: "$85.99",
+    price: "R1499.99",
   },
   {
     id: 6,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6381/6381750_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Crystal PS4",
-    price: "$85.99",
+    price: "R1499.99",
   },
   {
     id: 7,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6381/6381891_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Gold 2 PS4",
-    price: "$89.99",
+    price: "R1499.99",
   },
   {
     id: 8,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6517/6517324_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Vitrix PS4 ",
-    price: "$129.99",
+    price: "R1499.99",
   },
   {
     id: 9,
     imageurl:
       "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6564/6564742_sd.jpg;maxHeight=640;maxWidth=550",
     name: "Nacon PS4",
-    price: "$199.99",
+    price: "R1499.99",
   },
 ];
