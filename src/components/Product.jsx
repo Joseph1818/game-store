dd5699985b90442476dbec8ee551f6c1310ae90f
Umba joseph <Josephdjamba04@gmail.com>
2024-08-28 14:09:15 +0200

    Testing Card Image size on mobile V4

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ import "./Product.scss";
 export default function Product(props) {
   return (
     <div className="card">
-      {/* <img className="product--image" src={props.url} alt="product image" /> */}
+      <img className="product--image" src={props.url} alt="product image" />
       <h2 className="pruduct--name">{props.name}</h2>
       <p className="price">{props.price}</p>
       <p>{props.description}</p>
